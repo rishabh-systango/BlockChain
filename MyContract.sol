b816abe5a0051f89e01593b5ad01bb9e4b68f9e5
Rishabh Shah <rishabh@systango.com>
2018-01-30 14:32:18 +0530

    Initial Commit

---
+++ MyContract.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.0;
+
+contract MyContract{
+
+    uint myVariable;
+    address owner;
+
+    modifier onlyowner(){
+        if(owner == msg.sender){
+            _;
+        }else{
+            throw;
+        }
+    }
+
+    function MyContract() payable{
+        myVariable = 5;
+        owner = msg.sender;
+    }
+
+    function setMyVariable(uint myNewVariable) onlyowner{
+        myVariable = myNewVariable;
+    }
+
+    function getMyVariable() constant returns (uint){
+        return myVariable;
+    }
+
+    function getMyContractBalance() constant returns (uint){
+        return this.balance;
+    }
+
+    function kill() onlyowner{
+        suicide(owner);
+    }
+
+    function() payable{
+
+    }
+}
---
+++ mortal.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+
+contract mortal{
+    address owner;
+
+    modifier onlyowner(){
+        if(owner == msg.sender){
+            _;
+        }else{
+            throw;
+        }
+    }
+
+    function mortal(){
+        owner = msg.sender;
+    }
+
+    function kill() onlyowner{
+        suicide(owner);
+    }
+}
